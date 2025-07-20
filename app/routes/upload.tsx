e0a96b2233168381e916054a8811a80fa245887a
yosef <yosefpetrosh@gmail.com>
2025-07-20 09:25:48 -0500

    resume page details

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -58,7 +58,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
         console.log(data);
-        // navigate(`/resume/${uuid}`);
+        navigate(`/resume/${uuid}`);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
